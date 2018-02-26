1dd9a1b0776947d09f8fcafb854dda2f2110da31
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-26 20:40:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,7 +96,7 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, id).map { |row| self.new_from_db(row) }
+    DB[:conn].execute(sql, id).map { |row| self.new_from_db(row) }.first
     binding.pry
   end
 
