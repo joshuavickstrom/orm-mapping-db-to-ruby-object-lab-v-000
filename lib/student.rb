54a2bf6703228390ed8bca9ce3ce5d60f4c9d204
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-26 19:51:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
       FROM students
     SQL
 
-    DB[:conn].execute(sql, self.new_from_db(row))
+    DB[:conn].execute(sql, name).map do |row| self.new_from_db(row) end.first
   end
 
   def self.find_by_name(name)
