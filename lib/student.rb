f7304dcb043db39d3cbafa415a4c7666538b8ed0
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-26 20:27:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,18 +87,18 @@ class Student
 
     DB[:conn].execute(sql, num)
   end
-  #
-  # def self.first_student_in_grade_10
-  #   sql = <<-SQL
-  #     SELECT *
-  #     FROM students
-  #     WHERE grade =10
-  #     LIMIT 1
-  #   SQL
-  #
-  #   DB[:conn].execute(sql)
-  # end
-  #
+  
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade =10
+      LIMIT 1
+    SQL
+  
+    DB[:conn].execute(sql)
+  end
+  
   # def self.all_students_in_grade_X(num)
   #   sql = <<-SQL
   #       SELECT *
