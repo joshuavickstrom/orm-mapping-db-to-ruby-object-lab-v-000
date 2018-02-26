35f58993d45a17c1fca3bdda9bf85d8d8af29ee2
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-26 19:48:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
       FROM students
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql, self.new_from_db(row))
   end
 
   def self.find_by_name(name)
@@ -77,7 +77,10 @@ class Student
 
   def self.first_X_students_in_grade_10(X)
     sql = <<-SQL
-
+      SELECT *
+      FROM students
+      WHERE grade =10
+      LIMIT X 
     SQL
 
     DB[:conn].execute(sql)
