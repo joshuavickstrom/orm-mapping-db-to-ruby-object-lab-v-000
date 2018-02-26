feb60c508987132fa720172703877d06cf5d5c50
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-26 20:21:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,9 +28,7 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}.first
   end
 
   def save
