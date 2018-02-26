afe23ed673a403931a49c585ec4dfb945febc0c8
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-26 19:39:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,10 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-
+      SELECT *
+      FROM students
+      WHERE grade =10
+      LIMIT 1
     SQL
 
     DB[:conn].execute(sql)
