26970f0613540378df097c0e95f39232f747551e
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-02-26 19:39:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,4 +82,20 @@ class Student
 
     DB[:conn].execute(sql)
   end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.all_students_in_grade_X(X)
+    sql = <<-SQL
+
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 end
